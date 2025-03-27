54180a1c491611c910b774cbbbdafcc62ea6b152
Mrunal Tarwatkar <mrunatarwatkar018@gmail.com>
2025-03-27 17:26:18 +0530

    Server setup

--- api/server.js
+++ api/server.js
@@ -6,6 +6,15 @@ const cookieparser = require('cookie-parser');
 
 
 const app = express();
+app.use(cors());
+app.use(cookieparser());
+
+// MONGODB CONNECTION
+mongoose.connect('mongodb://localhost:27017/schoolManagemnt2025').then(db => {
+    console.log('MongoDB is connected Successfully.');
+}).catch(e => {
+    console.log('MongoDB error:', e);
+})
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
